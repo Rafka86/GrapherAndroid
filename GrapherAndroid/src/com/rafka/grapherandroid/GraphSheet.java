e87371213bf9ce291e815a5120d19b4b4b4f17dc
Rafka <fw.ge-mu.930@jb4.so-net.ne.jp>
2016-10-26 11:05:03 +0900

    Movable grid.

--- GrapherAndroid/src/com/rafka/grapherandroid/GraphSheet.java
+++ GrapherAndroid/src/com/rafka/grapherandroid/GraphSheet.java
@@ -22,18 +22,18 @@ public class GraphSheet extends View implements Observer, OnScaleGestureListener
 
 	private ScaleGestureDetector sDetector;
 	private EditText et1;
-	
+
 	private float gridSpan;
 
 	public GraphSheet(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		activity = (MainActivity) context;
 		sDetector = new ScaleGestureDetector(activity, this);
-		
+
 		paint = new Paint();
 		gridSpan = 1.0f;
 	}
-	
+
 	public void setGraphCore(GrapherCore gc) {
 		this.gc = gc;
 		this.gc.addObserver(this);
@@ -51,65 +51,85 @@ public class GraphSheet extends View implements Observer, OnScaleGestureListener
 		et1.append(s);
 		et1.append("\n");
 	}
-	
+
 	private void drawGrid(Canvas canvas) {
-		float width = (float)this.getWidth();
-		float height = (float)this.getHeight();
+		float width = gc.getViewWidth();
+		float height = gc.getViewHeight();
 		FloatBuffer fb = FloatBuffer.allocate(400);
 		fb.clear();
-		
-		float x_min = gc.getXMin(), x_max = gc.getXMax(), y_max = gc.getYMax(), y_min = gc.getYMin();
+
+		float x_min = gc.getXMin(), y_max = gc.getYMax();
 		float delta_x = gc.getDeltaX(), delta_y = gc.getDeltaY();
 		float gridSpanPix = gridSpan / delta_x;
-		
-		float xAyP =  y_max / delta_y; //X軸のY座標 x axis y pos
+
+		float xAyP = y_max / delta_y; //X軸のY座標 x axis y pos
 		float yAxP = -x_min / delta_x; //Y軸のX座標 y axis x pos
-		
+
 		//軸の描画
-		fb.put(yAxP);		fb.put(0.0f);
-		fb.put(yAxP);		fb.put(height);
-		fb.put(0.0f);		fb.put(xAyP);
-		fb.put(width);	fb.put(xAyP);
+		fb.put(yAxP);
+		fb.put(0.0f);
+		fb.put(yAxP);
+		fb.put(height);
+		fb.put(0.0f);
+		fb.put(xAyP);
+		fb.put(width);
+		fb.put(xAyP);
 		paint.setStrokeWidth(3.0f);
 		canvas.drawLines(fb.array(), paint);
 		fb.clear();
-		
-		for(float i = yAxP; i > 0.0f; i -= gridSpanPix) {
-			if (i <= 0.0f) break;
-			else if (i > width) i -= gridSpanPix * (int)((i - width) / gridSpanPix);
-			fb.put(i); fb.put(0.0f);
-			fb.put(i); fb.put(height);
-		}
-		
-		for(float i = yAxP; i < width; i += gridSpanPix) {
-			if (i >= width) break;
-			else if (i < 0.0f) i += gridSpanPix * (int)(-i / gridSpanPix);
-			fb.put(i); fb.put(0.0f);
-			fb.put(i); fb.put(height);
+
+		for (float i = yAxP; i > 0.0f; i -= gridSpanPix) {
+			if (i <= 0.0f)
+				break;
+			else if (i > width)
+				i -= gridSpanPix * (int) ((i - width) / gridSpanPix);
+			fb.put(i);
+			fb.put(0.0f);
+			fb.put(i);
+			fb.put(height);
 		}
 
-		for(float i = xAyP; i < height; i += gridSpanPix) {
-			if (i >= height) break;
-			else if (i < 0.0f) i += gridSpanPix * (int)(-i / gridSpanPix);
-			fb.put(0.0f);		fb.put(i);
-			fb.put(width);	fb.put(i);
+		for (float i = yAxP; i < width; i += gridSpanPix) {
+			if (i >= width)
+				break;
+			else if (i < 0.0f)
+				i += gridSpanPix * (int) (-i / gridSpanPix);
+			fb.put(i);
+			fb.put(0.0f);
+			fb.put(i);
+			fb.put(height);
 		}
-		
-		for(float i = xAyP; i > 0.0f; i -= gridSpanPix) {
-			if (i <= 0.0f) break;
-			else if (i > height) i -= gridSpanPix * (int)((i - height) / gridSpanPix);
-			fb.put(0.0f);		fb.put(i);
-			fb.put(width);	fb.put(i);
+
+		for (float i = xAyP; i < height; i += gridSpanPix) {
+			if (i >= height)
+				break;
+			else if (i < 0.0f)
+				i += gridSpanPix * (int) (-i / gridSpanPix);
+			fb.put(0.0f);
+			fb.put(i);
+			fb.put(width);
+			fb.put(i);
 		}
-		
+
+		for (float i = xAyP; i > 0.0f; i -= gridSpanPix) {
+			if (i <= 0.0f)
+				break;
+			else if (i > height)
+				i -= gridSpanPix * (int) ((i - height) / gridSpanPix);
+			fb.put(0.0f);
+			fb.put(i);
+			fb.put(width);
+			fb.put(i);
+		}
+
 		paint.setStrokeWidth(1.0f);
 		canvas.drawLines(fb.array(), paint);
 	}
-	
+
 	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
-		
+
 		drawGrid(canvas);
 	}
 
@@ -119,7 +139,17 @@ public class GraphSheet extends View implements Observer, OnScaleGestureListener
 		super.onTouchEvent(event);
 		sDetector.onTouchEvent(event);
 
-		invalidate();
+		if (event.getPointerCount() == 1) {
+			float touchX = gc.getXMin() + event.getX() * gc.getDeltaX();
+			float touchY = gc.getYMin() + gc.getYSize() - event.getY() * gc.getDeltaY();
+
+			switch (event.getAction()) {
+			case MotionEvent.ACTION_MOVE:
+				gc.setCenter(touchX, touchY);
+				break;
+			}
+		}
+
 		return true;
 	}
 
--- GrapherAndroid/src/com/rafka/grapherandroid/GrapherCore.java
+++ GrapherAndroid/src/com/rafka/grapherandroid/GrapherCore.java
@@ -4,81 +4,104 @@ import java.util.Observable;
 
 public class GrapherCore extends Observable {
 	private boolean viewSizeFlag;
-	
+
+	private float xSize, ySize;
 	private float xMax, xMin, yMax, yMin;
 	private float deltaX, deltaY;
-	private float centerY;
+	private float centerX, centerY;
 	private float vWidth, vHeight, vRate;
-	
+
 	public GrapherCore() {
-		centerY = 0.0f;
+		centerX = centerY = 0.0f;
+		xSize = 10.0f;
 		xMax = 5.0f;
 		xMin = -5.0f;
 		viewSizeFlag = false;
 	}
-	
-	public void setXMax(float value) {
-		xMax = value;
+
+	public void setCenter(float xVal, float yVal) {
+		centerX = xVal;
+		centerY = yVal;
 		CalcArgs();
-		
+
 		Changed();
 	}
-	
+
+	public float getCenterX() {
+		return centerX;
+	}
+
+	public float getCenterY() {
+		return centerY;
+	}
+
 	public float getXMax() {
 		return xMax;
 	}
-	
-	public void setXMin(float value) {
-		xMin = value;
-		CalcArgs();
-		
-		Changed();
-	}
-	
+
 	public float getXMin() {
 		return xMin;
 	}
-	
+
 	public float getYMax() {
 		return yMax;
 	}
-	
+
 	public float getYMin() {
 		return yMin;
 	}
-	
+
 	public float getDeltaX() {
 		return deltaX;
 	}
-	
+
 	public float getDeltaY() {
 		return deltaY;
 	}
-	
+
+	public float getViewWidth() {
+		return vWidth;
+	}
+
+	public float getViewHeight() {
+		return vHeight;
+	}
+
+	public float getXSize() {
+		return xSize;
+	}
+
+	public float getYSize() {
+		return ySize;
+	}
+
 	private void CalcArgs() {
-		float ySize = (xMax - xMin) * vRate;
+		ySize = xSize * vRate;
+
+		xMax = centerX + xSize / 2.0f;
+		xMin = centerX - xSize / 2.0f;
 		yMax = centerY + ySize / 2.0f;
 		yMin = centerY - ySize / 2.0f;
-		
-		deltaX = (xMax - xMin) / vWidth;
+
+		deltaX = xSize / vWidth;
 		deltaY = ySize / vHeight;
 	}
-	
+
 	public void setViewSize(int width, int height) {
-		vWidth = (float)width;
-		vHeight = (float)height;
+		vWidth = (float) width;
+		vHeight = (float) height;
 		vRate = vHeight / vWidth;
-		
-		CalcArgs();	
+
+		CalcArgs();
 		viewSizeFlag = true;
-		
+
 		Changed();
 	}
-	
+
 	public boolean hasViewSize() {
 		return viewSizeFlag;
 	}
-	
+
 	private void Changed() {
 		this.setChanged();
 		this.notifyObservers();
