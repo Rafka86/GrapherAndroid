2561cb60a0157b484bde067e4d5114283efe15a9
Rafka <fw.ge-mu.930@jb4.so-net.ne.jp>
2016-11-08 17:46:00 +0900

    画面遷移を実装。

--- GrapherAndroid/src/com/rafka/grapherandroid/MainActivity.java
+++ GrapherAndroid/src/com/rafka/grapherandroid/MainActivity.java
@@ -3,12 +3,16 @@ package com.rafka.grapherandroid;
 import com.rafka.grapherandroid.core.GrapherCore;
 import com.rafka.grapherandroid.parts.GraphSheet;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 
 public class MainActivity extends Activity {
+	private boolean nowEditScene = false;
+	
 	GrapherCore gc;
 	GraphSheet gs;
 	EditText et1;
@@ -20,6 +24,20 @@ public class MainActivity extends Activity {
 		gs = (GraphSheet) findViewById(R.id.graphSheet1);
 		gs.setEditText(et1);
 		gs.setGraphCore(gc);
+		
+		ActionBar ab = getActionBar();
+		ab.setDisplayHomeAsUpEnabled(false);
+		nowEditScene = false;
+		invalidateOptionsMenu();
+	}
+	
+	private void setGraphEditActivity() {
+		setContentView(R.layout.activity_sub);
+		
+		ActionBar ab = getActionBar();
+		ab.setDisplayHomeAsUpEnabled(true);
+		nowEditScene = true;
+		invalidateOptionsMenu();
 	}
 
 	@Override
@@ -31,7 +49,8 @@ public class MainActivity extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.option_menu, menu);
+		if (nowEditScene) getMenuInflater().inflate(R.menu.option_menu_sub, menu);
+		else getMenuInflater().inflate(R.menu.option_menu_main, menu);
 		return super.onCreateOptionsMenu(menu);
 	}
 
@@ -40,5 +59,19 @@ public class MainActivity extends Activity {
 		super.onWindowFocusChanged(hasFocus);
 		if (!gc.hasViewSize())
 			gc.setViewSize(gs.getWidth(), gs.getHeight());
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch(item.getItemId()) {
+			case android.R.id.home:
+				setMainActivity();
+				return true;
+			case R.id.menu_edit:
+				setGraphEditActivity();
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
 	}
 }
