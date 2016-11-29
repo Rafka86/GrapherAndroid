8ada9048ddc762e2e3c67bc466d82b8a11026879
Rafka <fw.ge-mu.930@jb4.so-net.ne.jp>
2016-11-29 17:38:38 +0900

    Add ColorChooser Class.

---
+++ GrapherAndroid/src/com/rafka/grapherandroid/parts/MyColorChooser.java
@@ -0,0 +1,14 @@
+package com.rafka.grapherandroid.parts;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class MyColorChooser extends View {
+
+	public MyColorChooser(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+}
